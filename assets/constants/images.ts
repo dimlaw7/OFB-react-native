371f78651dc587985e41c4f2a2a583da0d2971af
dimlaw7 <dimlaw7@gmail.com>
2025-01-14 09:34:28 +0100

    Assets setup and configuration
    Images and exports configuration.

---
+++ assets/constants/images.ts
@@ -0,0 +1,6 @@
+import foodBasket from "@/assets/images/icon.png";
+import password from "@/assets/images/password-icon.png";
+import phone from "@/assets/images/phone-icon.png";
+import arrowBack from "@/assets/images/arrow-back-icon.png";
+
+export default { foodBasket, password, phone, arrowBack };
---
+++ image.d.ts
@@ -0,0 +1,4 @@
+declare module "*.png" {
+  value: any;
+  export default value;
+}
