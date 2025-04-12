4214fd739255c20b5148ec1aa4a0262f71acc5b2
dimlaw7 <dimlaw7@gmail.com>
2025-04-12 13:13:03 +0100

    feat: update Index screen with user welcome UI and dashboard components
    - Added user avatar, name, and greeting to top of the home screen
    - Integrated WalletPackage, UpgradePackage, SavingsCard, and TransactionsComponent
    - Enhanced layout using ScrollView and SafeAreaView for proper spacing and scrollability
    - Applied custom fonts and consistent spacing for a clean dashboard look

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,21 +1,67 @@
-import { Link } from "expo-router";
-import { StyleSheet, Text } from "react-native";
+import icons from "@/assets/constants/icons";
+import images from "@/assets/constants/images";
+import SavingsCard from "@/components/SavingsCard";
+import TransactionsComponent from "@/components/TransactionsComponent";
+import UpgradePackage from "@/components/UpgradeCard";
+import WalletPackage from "@/components/WalletCard";
+import {
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <SafeAreaView style={styles.container}>
-      <Text style={{ fontFamily: "sf-semibolditalic" }}>
-        Welcome to OFB App!
-      </Text>
+    <SafeAreaView edges={["top"]}>
+      <ScrollView>
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between",
+            paddingHorizontal: 20,
+            paddingVertical: 14,
+            backgroundColor: "#FFFFFF",
+          }}
+        >
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <Image
+              source={images.avatar}
+              style={{ width: 48, height: 48, borderRadius: 100 }}
+            />
+            <View style={{ marginLeft: 8, gap: 4 }}>
+              <Text
+                style={{
+                  fontFamily: "sf-meadiun",
+                  color: "#666876",
+                }}
+              >
+                Welcome Back 👋
+              </Text>
+              <Text style={{ fontFamily: "sf-bold", fontSize: 14 }}>
+                Oladimeji Lawal
+              </Text>
+            </View>
+          </View>
+          <Image source={icons.bell} />
+        </View>
+        <View style={{ marginHorizontal: 16 }}>
+          <WalletPackage />
+          <UpgradePackage />
+          <SavingsCard />
+          <View>
+            <TransactionsComponent />
+          </View>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    height: "100%",
-    justifyContent: "center",
-    alignItems: "center",
-  },
+  container: {},
 });
